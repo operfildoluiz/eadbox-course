e561fa25b0f8ba850a3914e0124b58adae555a79
Luiz Gonçalves <luiz.goncalves@ufpr.br>
2018-07-16 22:54:00 -0300

    disable proxy and use cors-friendly api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
+/**
+ * As EADBOX API has CORS protection enabled and wasn't possible (due the challenge), was necessary to use a proxy to acces the server.
+ */
 export const environment = {
   production: true,
-  apiUrl: "/api/",
-  // apiUrl: "http://localhost:3000/",
+  apiUrl:
+    "https://cors-anywhere.herokuapp.com/https://luiz-teste.eadbox.com/api/",
   name: "TestSchool"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,9 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `angular-cli.json`.
-
+/**
+ * As EADBOX API has CORS protection enabled and wasn't possible (due the challenge), was necessary to use a proxy to acces the server.
+ */
 export const environment = {
   production: false,
-  apiUrl: "/api/",
-  // apiUrl: "http://localhost:3000/",
+  apiUrl:
+    "https://cors-anywhere.herokuapp.com/https://luiz-teste.eadbox.com/api/",
   name: "TestSchool"
 };
